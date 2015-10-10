6c693c1ba2c5b17910259ab82eb871747ca27ea2
ninjaparade <yazjallad@gmail.com>
2015-10-09 23:59:02 -1000

    added contact frontend controller

---
+++ src/Http/Controllers/Frontend/ContactController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pta\Contact\Http\Controllers\Frontend;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+
+class ContactController extends Controller
+{
+    use DispatchesJobs, ValidatesRequests;
+    
+    public function contact()
+    {
+      
+    }
+
+    public function contact_process()
+    {
+      
+    }
+
+}
