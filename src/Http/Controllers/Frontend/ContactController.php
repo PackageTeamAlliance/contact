de819a9192496de7052999212c098c9bc422b76e
ninjaparade <yazjallad@gmail.com>
2015-10-11 16:24:53 -0700

    return contact form

--- src/Http/Controllers/Frontend/ContactController.php
+++ src/Http/Controllers/Frontend/ContactController.php
@@ -9,15 +9,13 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 class ContactController extends Controller
 {
     use DispatchesJobs, ValidatesRequests;
-    
+
     public function contact()
     {
-      
+        return view('pta/contact::frontend.contact');
     }
 
     public function contact_process()
     {
-      
     }
-
 }
