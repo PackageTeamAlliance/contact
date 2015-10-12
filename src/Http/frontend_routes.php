1457f18881c970d0340bf63326a8572a0e917e7e
ninjaparade <yazjallad@gmail.com>
2015-10-11 18:14:50 -0700

    updated frontend routes

--- src/Http/frontend_routes.php
+++ src/Http/frontend_routes.php
@@ -1,6 +1,6 @@
 <?php
 
 $router->group(['namespace' => 'Pta\Contact\Http\Controllers\Frontend'], function () use ($router) {
-
   $router->get(config('contact.contact_page_uri'),  ['as' => 'contact.frontend', 'uses' => 'ContactController@contact']);
+  $router->post(config('contact.contact_page_uri'),  ['as' => 'contact.frontend.process', 'uses' => 'ContactController@contact_process']);
 });
